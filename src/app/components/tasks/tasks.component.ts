a73e960437bc67479b37639f382fec202f464d2f
mateus <mateusfrazao12345@gmail.com>
2025-03-26 11:08:19 -0300

    adicionando o repositorio remoto

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Tarefa } from '../../interfaces/Tarefa';
+@Component({
+  selector: 'app-tasks',
+  imports: [],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent implements OnInit {
+       tarefas: Tarefa[] =[]
+   constructor( private taskService:TaskService){}
+
+       ngOnInit(): void {
+           this.taskService.getTasks().subscribe((dado)=>{
+            this.tarefas = dado;
+            console.log(dado);
+           })
+          
+       }
+}
