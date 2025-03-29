85ecc13a372f9514dcc4fbedefb0f37d9354724c
mateus <mateusfrazao12345@gmail.com>
2025-03-29 16:32:22 -0300

    Finalizando os metodos de Get e Delete

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Tarefa } from '../../interfaces/Tarefa';
+import { faFontAwesome, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
-
+   @Input () tarefa!:Tarefa;//recebendo os dados do component pai que está com o service em funcionamento 
+   @Output() onDeleteTask = new EventEmitter<Tarefa>() //enviando os dados para o componente pai
+   faTimes = faTimes
+   
+   //criando um metodo para realizar o delete
+   onDelete(tarefa: Tarefa){
+     this.onDeleteTask.emit(tarefa);
+   }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { Tarefa } from '../../interfaces/Tarefa';
+import { CommonModule } from '@angular/common';
+import { TaskItemComponent } from "../task-item/task-item.component";
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [CommonModule, TaskItemComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent implements OnInit {
-       tarefas: Tarefa[] =[]
-   constructor( private taskService:TaskService){}
+       tarefas: Tarefa[] =[] //usando o OnInit para informar que é carregado no começo da pagina 
 
+   constructor( private taskService:TaskService){} //realizando a injeção de dependencia
        ngOnInit(): void {
            this.taskService.getTasks().subscribe((dado)=>{
             this.tarefas = dado;
             console.log(dado);
            })
-          
+       }
+
+
+       deleteTask(tarefa: Tarefa){
+          this.taskService.deleteTask(tarefa).subscribe();
        }
 }
