c1c4df953ec5aa97646ea57f0cc78200f3cc066d
mateus <mateusfrazao12345@gmail.com>
2025-09-20 14:24:34 -0300

    finalizado a logica do component

--- src/app/components/formlogin/formlogin.component.ts
+++ src/app/components/formlogin/formlogin.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-formlogin',
   imports: [],
   templateUrl: './formlogin.component.html',
-  styleUrl: './formlogin.component.css'
+  styleUrl: './formlogin.component.css',
 })
 export class FormloginComponent {
-
+  @Input() type: 'login' | 'register' = 'login';
 }
