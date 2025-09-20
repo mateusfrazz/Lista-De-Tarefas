1d65002157256a7f5edcc1b285c3fad6c42b4f64
mateus <mateusfrazao12345@gmail.com>
2025-09-20 14:16:00 -0300

    finalizando a estilização

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginTelaComponent } from './pages/login-tela/login-tela.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginTelaComponent },
+];
